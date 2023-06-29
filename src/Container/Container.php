b1a283d3c0d860f3fa2eeee55babe5ff47367de7
Ikoncoders <ikoncoders@gmail.com>
2023-06-29 12:27:24 +0400

    add dependency injection part one

---
+++ src/Container/Container.php
@@ -0,0 +1,101 @@
+<?php 
+namespace Ikoncoders\Framework\Container;
+
+use Psr\Container\ContainerInterface;
+use Ikoncoders\Framework\Exception\ContainerException;
+
+class Container implements ContainerInterface
+	{
+        private array $services = [];
+        /**
+         * add container id
+         */
+        public function add(string $id, string|object $concrete = null)
+        {
+            if (null === $concrete) {
+                if (!class_exists($id)) {
+                    throw new ContainerException("Service $id could not be added");
+                }
+
+                $concrete = $id;
+            }
+
+            $this->services[$id] = $concrete;
+        }
+
+        /**
+         * check if container has a particular id
+         */
+        public function has(string $id): bool
+        {
+            return array_key_exists($id, $this->services);
+        }
+
+         /**
+         * get container id
+         */
+        public function get(string $id)
+        {
+            if (!$this->has($id)) {
+                if (!class_exists($id)) {
+                    throw new ContainerException("Service $id could not be resolved");
+                }
+    
+                $this->add($id);
+            }
+    
+            $object = $this->resolve($this->services[$id]);
+    
+            return $object;
+        }
+
+        private function resolve($class): object
+        {
+            // 1. Instantiate a Reflection class (dump and check)
+            $reflectionClass = new \ReflectionClass($class);
+    
+            // 2. Use Reflection to try to obtain a class constructor
+            $constructor = $reflectionClass->getConstructor();
+    
+            // 3. If there is no constructor, simply instantiate
+            if (null === $constructor) {
+                return $reflectionClass->newInstance();
+            }
+    
+            // 4. Get the constructor parameters
+            $constructorParams = $constructor->getParameters();
+    
+            // 5. Obtain dependencies
+            $classDependencies = $this->resolveClassDependencies($constructorParams);
+    
+            // 6. Instantiate with dependencies
+            $service = $reflectionClass->newInstanceArgs($classDependencies);
+    
+            // 7. Return the object
+            return $service;
+        }
+
+        private function resolveClassDependencies(array $reflectionParameters): array
+    {
+        // 1. Initialize empty dependencies array (required by newInstanceArgs)
+        $classDependencies = [];
+
+        // 2. Try to locate and instantiate each parameter
+        /** @var \ReflectionParameter $parameter */
+        foreach ($reflectionParameters as $parameter) {
+
+            // Get the parameter's ReflectionNamedType as $serviceType
+            $serviceType = $parameter->getType();
+
+            // Try to instantiate using $serviceType's name
+            $service = $this->get($serviceType->getName());
+
+            // Add the service to the classDependencies array
+            $classDependencies[] = $service;
+        }
+
+        // 3. Return the classDependencies array
+        return $classDependencies;
+    }
+
+	}
---
+++ src/Exception/ContainerException.php
@@ -0,0 +1,10 @@
+<?php // framework/src/Container/ContainerException.php
+namespace Ikoncoders\Framework\Exception;
+
+use Psr\Container\ContainerExceptionInterface;
+
+
+class ContainerException extends \Exception implements ContainerExceptionInterface
+{
+
+}
